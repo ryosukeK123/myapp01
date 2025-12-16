6f563f048a3bf5570d9af392a6c73cec2efd843e
RYOSUKE <spheniscidae82@gmail.com>
2025-12-17 00:12:54 +0900

    レイアウト調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path   # ログイン画面に戻す
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
