944e835aee237c4ff1c95d3cde4e7a0446c95fc3
RYOSUKE <spheniscidae82@gmail.com>
2026-01-05 23:35:15 +0900

    rspecの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,45 @@ RSpec.describe User, type: :model do
   describe 'バリデーション' do
     context '名前とメールがあるとき' do
       it '有効であること' do
-        user = User.new(name: '太郎', email: 'taro@example.com')
+        user = User.new(
+          name: '太郎',
+          email: 'taro@example.com',
+          password: 'password',
+          password_confirmation: 'password'
+        )
         expect(user).to be_valid
+      end
+    end
+
+    context '名前がないとき' do
+      it '無効であること' do
+        user = User.new(
+          name: nil,
+          email: 'taro@example.com',
+          password: 'password',
+          password_confirmation: 'password'
+        )
+         expect(user.valid?).to be false
+      end
+    end
+
+    context 'メールが重複しているとき' do
+      it '無効であること' do
+        User.create!(
+          name: '太郎',
+          email: 'taro@example.com',
+          password: 'password',
+          password_confirmation: 'password'
+        )
+
+        user = User.new(
+          name: '次郎',
+          email: 'taro@example.com',
+          password: 'password',
+          password_confirmation: 'password'
+        )
+
+        expect(user).not_to be_valid
       end
     end
   end
